8484c393875323e191c295388b1f133fe04a901e
Vicente David Fleitas Mendoza <vicente.fleitas@h2oinnovation.com.py>
2025-05-18 20:21:10 -0300

    feat: simple count set and get from zustand store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,15 @@
 import {create} from "zustand"
 
-export const useCounterStore = create(() => ({
-    count:0
+export const useCounterStore = create((set) => ({
+    count:0,
+    increment: () => {
+        set((state) => ({count: state.count + 1}))
+    },
+    incrementAsync: async () => {
+        const response = await new Promise(resolve => setTimeout(resolve,1000))
+        set(state => ({count: state.count + 1}))
+    },
+    decrement: () => {
+        set((state) => ({count: state.count - 1}))
+    },
 }))
